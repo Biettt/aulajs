45b3876e6fb69775c1f19375d3632f1f48e33030
Biettt <rm551237@fiap.com.br>
2023-08-09 21:58:52 -0300

    lendo valor

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 //criando a variavel e setando a chave o valor
 let key='item1';
 //localStorage.setItem(key, 'valor'); 
-sessionStorage.setItem(key, 'valor');
+sessionStorage.setItem(key, 'Gabriela');
 
+
+//lendo o valor que já está
 let lerItem = localStorage.getItem(key);
 console.log(lerItem);
- 
+
 
